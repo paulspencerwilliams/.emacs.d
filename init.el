f9e582de3a55d27d40f9a134908b38520c39a7ad
Paul Williams <paul@paulswilliams.me.uk>
2021-09-11 10:48:43 +0100

    Seemingly better defaults
    Filtered and copied from https://github.com/suvratapte/dot-emacs-dot-d/blob/e7f44d981004b44bb9ae23b6b67c421404ea6b4e/init.el#L19

--- init.el
+++ init.el
@@ -1,6 +1,26 @@
-(setq inhibit-startup-message t)
+
+(setq-default
+ inhibit-startup-message t
+ custom-file "~/.emacs.d/custom-file.el"
+ fille-column 80
+ create-lockfiles nil
+ indent-tabs-mode nil
+ auto-save-default nil
+ enable-recursive-minibuffers t)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(column-number-mode t)
+(global-auto-revert-mode t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (global-hl-line-mode t)
 (line-number-mode t)
+
+(setq-default
+ x-select-enable-clipboard t
+ x-select-enable-primary t
+ save-interprogram-paste-before-kill t
+ apropos-do-all t)
