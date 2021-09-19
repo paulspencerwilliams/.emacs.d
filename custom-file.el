cca1030355c748637e2dab72b5db571b980a7c82
Paul Williams <paul@paulswilliams.me.uk>
2021-09-19 19:32:29 +0100

    marked JAVA_HOME as safe

--- custom-file.el
+++ custom-file.el
@@ -4,7 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(projectile ob-restclient restclient es-mode smartparens flycheck-clj-kondo exec-path-from-shell cider company spacemacs-theme magit use-package)))
+   '(projectile ob-restclient restclient es-mode smartparens flycheck-clj-kondo exec-path-from-shell cider company spacemacs-theme magit use-package))
+ '(safe-local-variable-values
+   '((eval setenv "JAVA_HOME" "/Library/Java/JavaVirtualMachines/temurin-11.jdk/Contents/Home")
+     (eval setenv "JAVA_HOME" "/Library/Java/JavaVirtualMachines/temurin-8.jdk/Contents/Home"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
